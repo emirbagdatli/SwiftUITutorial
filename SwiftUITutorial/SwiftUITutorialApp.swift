a469cf7ecae01dc21e80a839f83a4e098e518ec6
EMIR BAGDATLI <emirbgt@hotmail.com>
2024-02-12 16:05:41 +0300

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Emir Bağdatlı on 12.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
