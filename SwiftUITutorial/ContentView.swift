138028c5df8d1830d8b535a846b27e99f6d100ea
System Administrator <emirbgt@hotmail.com>
2024-02-12 19:21:35 +0300

    swift tutorial first commit

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -8,17 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    
+    var body: some View  {
+        sampleVerticalHorizontal()
+    }
+    
+    fileprivate func sampleVerticalHorizontal() -> some View {
+        return VStack(alignment: .center, content: {
+            AsyncImage(url: URL(string: "https://picsum.photos/400/300")
+            ).frame(width: 256,height: 256).clipShape(Circle())
+            Text(verbatim: "Username")
+            HStack(content: {
+                TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/).textFieldStyle(.roundedBorder.self)
+                Text("Data")
+            }
+            ).padding()
+        })
+    }
+    fileprivate func sampleZStack() -> some View {
+        return ZStack(
+            content: {
+                AsyncImage(url: URL(string: "https://picsum.photos/400/300"))
+                Text("Test")
+        })
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Preview : PreviewProvider {
+    static var  previews: some View {
+        ContentView()
+    }
 }
